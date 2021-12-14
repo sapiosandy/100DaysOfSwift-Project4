4a86dfab498c600dee6a608095cdeafcb787a5da
Sandy Gomez <sgomez1029@gmail.com>
2021-12-14 15:39:34 -0800

    Project4_100DaysOfSwift_Completed

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -9,37 +9,49 @@ import WebKit
 
 class ViewController: UIViewController{
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["levis.com", "bestbuy.com","victoriassecret.com"]
     
     override func loadView() {
-        // create an instance of Apple's WKWebView and assign it to the webView property:
-        webView = WKWebView()
-       
-        // set the web view's navigationDelegate property to self which means "when any web page naviagtion happens please tell me - the current view controller.
-        webView.navigationDelegate = self
         
-        // make our view (the root view of our view controller) that web view:
+        webView = WKWebView()
+        webView.navigationDelegate = self
         view = webView
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
-        
-        let url = URL(string:"https://www.levis.com")!
-        
-        // this creates a new URLRequest object from that URL, and gives it to our web view to load:
-        webView.load(URLRequest(url: url))
-        
     
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+    
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressButton,spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+
+        let url = URL(string:"https://" + websites[0])!
+        
+        webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
     @objc func openTapped() {
+        
         let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "levis.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "victoriassecret.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "bestbuy.com", style: .default, handler: openPage))
+        for website in websites{
+        ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        
+        ac.popoverPresentationController?.barButtonItem =
+        self.navigationItem.rightBarButtonItem
+        
         present(ac, animated: true)
     }
     func openPage(action: UIAlertAction) {
@@ -49,8 +61,26 @@ class ViewController: UIViewController{
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
     }
-}
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
 
+        decisionHandler(.cancel)
+    }
+}
 extension ViewController: WKNavigationDelegate {
     
 }
